b1c308406d78cadf3677944f5096a3e6a9f75f4b
Leonardo <leonardo@finalis.com>
2024-12-06 09:38:07 -0300

    fns(nest) baseline, to connect Auth0  by axios

---
+++ src/app.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { isEmpty } from 'lodash';
+
+@Injectable()
+export class AppService {
+  private axiosInstance: AxiosInstance;
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: process.env.AUTH0_QA_BASEURL,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  async setToken(): Promise<void> {
+    const response = await this.axiosInstance.post('/oauth/token', {
+      client_id: process.env.AUTH0_QA_CLIENT_ID,
+      client_secret: process.env.AUTH0_QA_CLIENT_SECRET,
+      audience: process.env.AUTH0_QA_AUDIENCE,
+      grant_type: 'client_credentials',
+    });
+
+    this.axiosInstance.defaults.headers.common[
+      'Authorization'
+    ] = `Bearer ${response.data.access_token}`;
+  }
+
+  async getUserIdByEmail(userEmail: string): Promise<[string, boolean]> {
+    const params = { q: `email="${userEmail}"`, search_engine: 'v3' };
+    const response = await this.axiosInstance.get('/api/v2/users', { params });
+    const userId = response?.data[0]?.user_id;
+    const blocked = response?.data[0]?.blocked;
+    return [userId, blocked];
+  }
+
+  async updateUserById(id: string): Promise<void> {
+    if (isEmpty(id)) return;
+    const encodeId = encodeURIComponent(id);
+    await this.axiosInstance.patch(`/api/v2/users/${encodeId}`, {
+      blocked: true,
+    });
+  }
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
