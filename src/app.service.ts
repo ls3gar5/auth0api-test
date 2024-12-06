6d74bc70adaa153c5647eab82c74d343baf2f846
Leonardo <leonardo@finalis.com>
2024-12-06 17:20:29 -0300

    feat(nest): refactor controller

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
-import { isEmpty } from 'lodash';
+import { EMAILS } from 'db/emails';
+import { chunk, isEmpty } from 'lodash';
 
 @Injectable()
 export class AppService {
@@ -27,6 +28,39 @@ export class AppService {
     ] = `Bearer ${response.data.access_token}`;
   }
 
+  getEmailListToProcess(): string[][] {
+    const emails: string[] = EMAILS[process.env.NODE_ENV];
+    const batches = chunk(emails, 8);
+    console.log('Amount of email to process: ', emails.length);
+    console.log('Batches data to process: ', batches.length);
+    return batches;
+  }
+
+  async getUsersIdList(batches: string[][]): Promise<string[]> {
+    const userIdsToUpdate: string[] = [];
+    let batchIndex = 1;
+    for (const batch of batches) {
+      console.log('Batch number =>: ', batchIndex);
+      await Promise.all(
+        batch.map(async (email) => {
+          try {
+            const [userId, isBlocked] = await this.getUserIdByEmail(email);
+            if (!isEmpty(userId) && !isBlocked) {
+              console.log(email);
+              userIdsToUpdate.push(userId);
+            }
+          } catch (error) {
+            console.error(`Error processing email ${email}:`, error);
+          }
+        }),
+      );
+      await this.delay();
+      batchIndex += 1;
+    }
+    console.log('Amount of users to update: ', userIdsToUpdate.length);
+    return userIdsToUpdate;
+  }
+
   async getUserIdByEmail(userEmail: string): Promise<[string, boolean]> {
     const params = { q: `email="${userEmail}"`, search_engine: 'v3' };
     const response = await this.axiosInstance.get('/api/v2/users', { params });
@@ -42,6 +76,45 @@ export class AppService {
       blocked: true,
     });
   }
+
+  async setUsersBlocked(userIdsToUpdate: string[]): Promise<string[]> {
+    const batches = chunk(userIdsToUpdate, 5);
+    console.log('batches to update users process', batches.length);
+
+    const batchIndex = 1;
+    const results = [];
+    for (const batch of batches) {
+      console.log('Batch number =>: ', batchIndex);
+      await Promise.all(
+        batch.map(async (userId) => {
+          try {
+            //3- BLOCKED USER BY USER ID
+            await this.updateUserById(userId);
+            results.push({
+              userId,
+              status: 'Updated',
+              error: 'OK',
+            });
+          } catch (error) {
+            console.error(`Error processing userId ${userId}:`, error);
+            results.push({
+              userId,
+              status: 'error',
+              error: error.message,
+            });
+          }
+        }),
+      );
+      await this.delay();
+    }
+    return results;
+  }
+
+  delay = () => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, 5000);
+    });
+  };
 
   getHello(): string {
     return 'Hello World!';
