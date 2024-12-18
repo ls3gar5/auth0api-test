1bf5dea5e7cd7730b1c8a39ade6168a558f0762e
Leonardo <leonardo@finalis.com>
2024-12-18 16:13:14 -0300

    feat(nest): add typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './users/users.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -9,6 +12,25 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true, // Makes ConfigModule available everywhere
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>(`DB_${process.env.NODE_ENV}_HOST`),
+        port: configService.get<number>(`DB_PORT`),
+        database: configService.get<string>('DB_NAME'),
+        username: configService.get<string>(
+          `DB_${process.env.NODE_ENV}_USERNAME`,
+        ),
+        password: configService.get<string>(
+          `DB_${process.env.NODE_ENV}_PASSWORD`,
+        ),
+        entities: [Users],
+        autoLoadEntities: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
